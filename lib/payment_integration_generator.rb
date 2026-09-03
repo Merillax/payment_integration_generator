3c971e401761b0b14294820449e915e1ac8c7a59
AlSerP <gosha1416@yandex.ru>
2026-09-03 23:47:14 +0300

    Базовая генерация класса интеграции переписана под актуальные условия

--- lib/payment_integration_generator.rb
+++ lib/payment_integration_generator.rb
@@ -3,11 +3,15 @@
 # === Gems ===
 require "erb"
 require "openapi3_parser"
+require "ostruct"
 
 # === Modules ===
 require_relative "payment_integration_generator/version"
 require_relative "payment_integration_generator/parsers/open_api_parser"
+require_relative "payment_integration_generator/generators/base_generator"
 require_relative "payment_integration_generator/generators/integration_generator"
+require_relative "payment_integration_generator/searchers/base_searcher"
+require_relative "payment_integration_generator/searchers/create_request_searcher"
 require_relative "payment_integration_generator/cli"
 
 module PaymentIntegrationGenerator
--- lib/payment_integration_generator/generators/integration_generator.rb
+++ lib/payment_integration_generator/generators/integration_generator.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
-require "erb"
-
 module PaymentIntegrationGenerator
-  class IntegrationGenerator
+  class IntegrationGenerator < BaseGenerator
     DEFAULT_OUTPUT_PATH = "lib/integrations"
+    PARTIAL_INDENT_SIZE = 4
+
     SUPPORTED_HTTP_METHODS = %i[get post put delete].freeze
 
+    PROVIDER_PUBLIC_METHODS = %i[create_request fetch_status process_callback check_conditions].freeze
+    PROVIDER_PRIVATE_METHODS = %i[build_payout_payload errors_mapping status_mapping].freeze
+
     def initialize(openapi_document:, integration_name:, output_folder_path: nil)
-      @openapi_document = openapi_document
-      @integration_name = integration_name
-      @output_folder_path = output_folder_path
+      super(openapi_document:, integration_name:, output_folder_path:)
     end
 
     def call
@@ -20,22 +21,9 @@ module PaymentIntegrationGenerator
 
     private
 
-    def render_template(template, output_path)
-      erb = ERB.new(template, trim_mode: "-")
-      content = erb.result(binding)
-
-      FileUtils.mkdir_p(File.dirname(output_path))
-      File.write(output_path, content)
-    end
-
     def generate_integration_class
       template = File.read(File.join(TEMPLATES_DIR, "class.erb"))
-      render_template(template, output_path + "/#{snake_case(@integration_name)}.rb")
-    end
-
-    def output_path
-      # TODO: default path should look for a Gemfile and then goes to DEFAULT_OUTPUT_PATH
-      @output_folder_path || DEFAULT_OUTPUT_PATH
+      render_template(template, output_path + "/#{snake_case(@integration_name)}_service.rb", binding)
     end
 
     # @param operation_item <Openapi3Parser::Node::Operation>
@@ -44,19 +32,8 @@ module PaymentIntegrationGenerator
       snake_case(operation_item.operation_id)
     end
 
-    def snake_case(str)
-      str.gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-         .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-         .tr("-", "_")
-         .gsub(/\s+/, "_")
-         .downcase
-    end
-
-    # @param operation_item <Openapi3Parser::Node::Operation>
-    # @return <String> args string for method. Example: "(id, order="desc")"
-    def collect_method_args(operation_item)
-      # TODO: collect args string
-      ""
+    def provider_default_url
+      @openapi_document.servers.first.url
     end
   end
 end
